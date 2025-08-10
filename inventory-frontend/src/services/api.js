e3613a141d6e30e23a241da97d34606ee9c6b6a8
baljinderwal <baljinderwarewaal@gmail.com>
2025-08-10 15:48:56 +0530

    pushed hanges

--- inventory-frontend/src/services/api.js
+++ inventory-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // The port where json-server will run
+  baseURL: 'http://localhost:3000', // The port where json-server will run
 });
 
 export default api;
